chocodev11/POMC

Add third-person camera and self-rendering
- F5 cycles: first person, third person back, third person front
- Camera collision raycast prevents clipping through blocks
- Own player model rendered as Steve in third-person modes
- Hand hidden in third-person

src/renderer/camera.rs
@@ -8,7 +8,7 @@ const NEAR: f32 = 0.1;
 const FAR: f32 = 1000.0;
 const SENSITIVITY: f32 = 0.003;
 const PITCH_LIMIT: f32 = std::f32::consts::FRAC_PI_2 - 0.01;
-const THIRD_PERSON_DISTANCE: f32 = 4.0;
+pub const THIRD_PERSON_DISTANCE: f32 = 4.0;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub enum CameraMode {
@@ -33,6 +33,7 @@ pub struct Camera {
     pub yaw: f32,
     pub pitch: f32,
     pub mode: CameraMode,
+    pub third_person_dist: f32,
     aspect_ratio: f32,
     fov_modifier: f32,
 }
@@ -45,6 +46,7 @@ impl Camera {
             yaw: 0.0,
             pitch: 0.0,
             mode: CameraMode::FirstPerson,
+            third_person_dist: THIRD_PERSON_DISTANCE,
             aspect_ratio,
             fov_modifier: 1.0,
         }
@@ -119,8 +121,8 @@ impl Camera {
     pub fn third_person_offset(&self) -> Vec3 {
         match self.mode {
             CameraMode::FirstPerson => Vec3::ZERO,
-            CameraMode::ThirdPersonBack => -self.forward() * THIRD_PERSON_DISTANCE,
-            CameraMode::ThirdPersonFront => self.forward() * THIRD_PERSON_DISTANCE,
+            CameraMode::ThirdPersonBack => -self.forward() * self.third_person_dist,
+            CameraMode::ThirdPersonFront => self.forward() * self.third_person_dist,
         }
     }
 

src/renderer/mod.rs
@@ -513,6 +513,38 @@ impl Renderer {
         self.camera.pitch = pitch;
     }
 
+    pub fn update_third_person_distance(
+        &mut self,
+        eye_pos: glam::Vec3,
+        chunks: &crate::world::chunk::ChunkStore,
+    ) {
+        if self.camera.mode == camera::CameraMode::FirstPerson {
+            return;
+        }
+        let dir = match self.camera.mode {
+            camera::CameraMode::ThirdPersonBack => -self.camera.forward(),
+            camera::CameraMode::ThirdPersonFront => self.camera.forward(),
+            _ => return,
+        };
+        let max = camera::THIRD_PERSON_DISTANCE;
+        let mut dist = max;
+        let step = 0.1;
+        let mut t = 0.0;
+        while t < max {
+            let p = eye_pos + dir * t;
+            let bx = p.x.floor() as i32;
+            let by = p.y.floor() as i32;
+            let bz = p.z.floor() as i32;
+            let state = chunks.get_block_state(bx, by, bz);
+            if !state.is_air() {
+                dist = (t - 0.2).max(0.0);
+                break;
+            }
+            t += step;
+        }
+        self.camera.third_person_dist = dist;
+    }
+
     pub fn update_fov(&mut self, sprinting: bool) {
         self.camera.update_fov_modifier(sprinting);
     }
@@ -529,7 +561,6 @@ impl Renderer {
         self.camera.mode = self.camera.mode.cycle();
     }
 
-    #[allow(dead_code)]
     pub fn is_first_person(&self) -> bool {
         self.camera.mode == camera::CameraMode::FirstPerson
     }

PommeMC/Pomme-Client

src/renderer/camera.rs
@@ -12,7 +12,7 @@ const NEAR: f32 = 0.1;
 const FAR: f32 = 1000.0;
 const SENSITIVITY: f32 = 0.003;
 const PITCH_LIMIT: f32 = std::f32::consts::FRAC_PI_2 - 0.01;
-const THIRD_PERSON_DISTANCE: f32 = 4.0;
+pub const THIRD_PERSON_DISTANCE: f32 = 4.0;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub enum CameraMode {
@@ -37,6 +37,7 @@ pub struct Camera {
     pub yaw: f32,
     pub pitch: f32,
     pub mode: CameraMode,
+    pub third_person_dist: f32,
     aspect_ratio: f32,
     pub base_fov_degrees: f32,
     fov_modifier: f32,
@@ -51,6 +52,7 @@ impl Camera {
             yaw: 0.0,
             pitch: 0.0,
             mode: CameraMode::FirstPerson,
+            third_person_dist: THIRD_PERSON_DISTANCE,
             aspect_ratio,
             base_fov_degrees: DEFAULT_FOV_DEGREES,
             fov_modifier: 1.0,
@@ -134,8 +136,8 @@ impl Camera {
     pub fn third_person_offset(&self) -> Vec3 {
         match self.mode {
             CameraMode::FirstPerson => Vec3::ZERO,
-            CameraMode::ThirdPersonBack => -self.forward_vec() * THIRD_PERSON_DISTANCE,
-            CameraMode::ThirdPersonFront => self.forward_vec() * THIRD_PERSON_DISTANCE,
+            CameraMode::ThirdPersonBack => -self.forward_vec() * self.third_person_dist,
+            CameraMode::ThirdPersonFront => self.forward_vec() * self.third_person_dist,
         }
     }
 

src/renderer/mod.rs
@@ -550,6 +550,38 @@ impl Renderer {
         self.camera.pitch = pitch;
     }
 
+    pub fn update_third_person_distance(
+        &mut self,
+        eye_pos: glam::Vec3,
+        chunks: &crate::world::chunk::ChunkStore,
+    ) {
+        if self.camera.mode == camera::CameraMode::FirstPerson {
+            return;
+        }
+        let dir = match self.camera.mode {
+            camera::CameraMode::ThirdPersonBack => -self.camera.forward_vec(),
+            camera::CameraMode::ThirdPersonFront => self.camera.forward_vec(),
+            _ => return,
+        };
+        let max = camera::THIRD_PERSON_DISTANCE;
+        let mut dist = max;
+        let step = 0.1;
+        let mut t = 0.0;
+        while t < max {
+            let p = eye_pos + dir * t;
+            let bx = p.x.floor() as i32;
+            let by = p.y.floor() as i32;
+            let bz = p.z.floor() as i32;
+            let state = chunks.get_block_state(bx, by, bz);
+            if !state.is_air() {
+                dist = (t - 0.2).max(0.0);
+                break;
+            }
+            t += step;
+        }
+        self.camera.third_person_dist = dist;
+    }
+
     pub fn update_fov(&mut self, modifier: f32) {
         self.camera.update_fov_modifier(modifier);
     }
@@ -570,7 +602,6 @@ impl Renderer {
         self.camera.mode = self.camera.mode.cycle();
     }
 
-    #[allow(dead_code)]
     pub fn is_first_person(&self) -> bool {
         self.camera.mode == camera::CameraMode::FirstPerson
     }